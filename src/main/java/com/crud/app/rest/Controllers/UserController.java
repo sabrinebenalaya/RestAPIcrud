03fe59831e0b2502942e17e41aaec02bbbc7b36f
sabrinebenalaya <benalayasabrine03@gmail.com>
2022-04-30 13:56:55 +0200

    this is my second commit

--- src/main/java/com/crud/app/rest/Controllers/UserController.java
+++ src/main/java/com/crud/app/rest/Controllers/UserController.java
@@ -2,7 +2,6 @@ package com.crud.app.rest.Controllers;
 
 import java.util.List;
 
-
 import com.crud.app.rest.Models.User;
 import com.crud.app.rest.Repos.UserRepos;
 
@@ -10,12 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 
 @Controller
 @RequestMapping(value = "/user")
@@ -47,24 +47,12 @@ public class UserController {
         return "User Created";
     }
 
-    //update  user
-    @PostMapping(path = "/update/{id}")
-    public @ResponseBody String updateUser(@RequestParam long id){
-     userrepos.deleteById(id);
-     return "user deleted";
-    }
-        
-    //delete  user
-    @PostMapping(path = "/delete/{id}")
-    public User deleteUser(@RequestParam int id, @RequestParam User user){
-    return userrepos.updateUserById(id,user);
-    }
-
     //trouver un user par son id
     @GetMapping(path = "/id")
-    public User getUserById (@RequestParam long id){
-                   return userrepos.findById(id);   
+    public  User  findById(@RequestParam long id) {
+        return userrepos.findById(id).orElse(null);
     }
+   
 
     //trouver un user par son occupation
     @GetMapping(path = "/occupation")
@@ -91,11 +79,28 @@ public class UserController {
     }
     
     //trouver un user par son nom et prenom
-    @GetMapping(path = "/name")
+    @GetMapping(path = "/username")
     public ResponseEntity<List<User>>  getUserByFirstName(@RequestParam String firstName, @RequestParam String lastName ){
-        return new ResponseEntity<List<User>>(userrepos.findByFirstNameANDLastName(firstName,lastName), HttpStatus.OK);
+        return new ResponseEntity<List<User>>(userrepos.findByFirstNameAndLastName(firstName,lastName), HttpStatus.OK);
     }
-
-  
+ 
+    //update  user
+    @PostMapping(path = "/update")
+    public String updateUserById(User user){
+        User userFromDb = userrepos.findById(user.getId()).orElse(null);
+        userFromDb.setFirstname(user.getFirstname()); 
+        userFromDb.setLastName(user.getLastName());
+        userFromDb.setAge(user.getAge());
+        userFromDb.setOccupation(user.getOccupation());
+        userrepos.save(userFromDb);
+        return "User updated";
+    }
+            
+        //delete  user
+        @DeleteMapping(path = "/delete/{id}")
+        public String deleteUserById(@PathVariable int id){
+            userrepos.deleteById((long) id);
+            return"User deleted";
+        }
     
 }
--- src/main/java/com/crud/app/rest/Repos/UserRepos.java
+++ src/main/java/com/crud/app/rest/Repos/UserRepos.java
@@ -5,25 +5,14 @@ import java.util.List;
 import com.crud.app.rest.Models.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface UserRepos extends JpaRepository<User, Long>{
-    User findById(long id);
     List<User> findByFirstName(String firstName);
     List<User> findByLastName(String lastName);
     List<User> findByOccupation(String occupation);
     List<User> findByAge(int age);
-    List<User> findByFirstNameANDLastName(String firstName,String lastName);
-    
-    public default User updateUserById(int id, User user){
-        User UserUpdate = findById(id);
-        UserUpdate.setFirstname(user.getFirstname());
-        UserUpdate.setLastName(user.getLastName());
-        UserUpdate.setAge(user.getAge());
-        UserUpdate.setOccupation(user.getOccupation());
-        return save(UserUpdate);
-    }
-
-  
-
+    List<User> findByFirstNameAndLastName(String firstName,String lastName);
+ 
 }
